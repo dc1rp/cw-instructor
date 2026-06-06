86ba56d31ce735687f38735f8f9d758909e75359
Philip Ridder <philip.ridder@gmail.com>
2026-06-06 18:51:32 +0200

    feat: restructure decoder entirely

---
+++ morse.js
@@ -0,0 +1,155 @@
+class StateMachine {
+    States = Object.freeze({
+        IDLE: 0,
+        KEY_DOWN: 1,
+        DOT: 2,
+        DASH: 3,
+        KEY_UP: 4,
+        CHARACTER_SPACE: 5,
+        WORD_SPACE: 6,
+        NEW_LINE: 7
+    });
+
+    Trigger = Object.freeze({
+        KEY_DOWN: 0,
+        KEY_UP: 1,
+        TIMEOUT: 2
+    });
+
+    Events = Object.freeze({
+        DASH: "dash",
+        DOT: "dot",
+        CHARACTER_SPACE: "characterSpace",
+        WORD_SPACE: "wordSpace",
+        NEW_LINE: "newLine"
+    });
+
+    dashEvent = new CustomEvent(this.Events.DASH);
+    dotEvent = new CustomEvent(this.Events.DOT);
+    characterSpaceEvent = new CustomEvent(this.Events.CHARACTER_SPACE);
+    wordSpaceEvent = new CustomEvent(this.Events.WORD_SPACE);
+    newLineEvent = new CustomEvent(this.Events.NEW_LINE);
+
+    timeout = null;
+
+    constructor(timeUnit = 100) {
+        this.timeUnit = timeUnit;
+        this.currentState = this.States.IDLE;
+    }
+
+    transition(event) {
+        switch (this.currentState) {
+            case this.States.IDLE:
+                if (event === this.Trigger.KEY_DOWN) {
+                    this.switch(this.States.KEY_DOWN);
+                }
+                break;
+            case this.States.KEY_DOWN:
+                if (event === this.Trigger.KEY_UP) {
+                    this.switch(this.States.DOT);
+                } else if (event === this.Trigger.TIMEOUT) {
+                    this.switch(this.States.DASH);
+                }
+                break;
+            case this.States.DOT:
+                break;
+            case this.States.DASH:
+                if (event === this.Trigger.KEY_UP) {
+                    this.switch(this.States.KEY_UP);
+                }
+                break;
+            case this.States.KEY_UP:
+                if (event === this.Trigger.TIMEOUT) {
+                    this.switch(this.States.CHARACTER_SPACE);
+                } else if (event === this.Trigger.KEY_DOWN) {
+                    this.switch(this.States.KEY_DOWN);
+                }
+                break;
+            case this.States.CHARACTER_SPACE:
+                if (event === this.Trigger.KEY_DOWN) {
+                    this.switch(this.States.KEY_DOWN);
+                } else if (event === this.Trigger.TIMEOUT) {
+                    this.switch(this.States.WORD_SPACE);
+                }
+                break;
+            case this.States.WORD_SPACE:
+                if (event === this.Trigger.KEY_DOWN) {
+                    this.switch(this.States.KEY_DOWN);
+                } else if (event === this.Trigger.TIMEOUT) {
+                    this.switch(this.States.NEW_LINE);
+                }
+                break;
+            case this.States.NEW_LINE:
+                break;
+        }
+    }
+
+    switch (state) {
+        this.leave(state);
+        this.currentState = state;
+        this.enter(state);
+    }
+    
+    leave(state) {
+        switch (state) {
+            case this.States.IDLE:
+                break;
+            case this.States.KEY_DOWN:
+                break;
+            case this.States.DOT:
+                break;
+            case this.States.DASH:
+                break;
+            case this.States.KEY_UP:
+                break;
+            case this.States.CHARACTER_SPACE:
+                break;
+            case this.States.WORD_SPACE:
+                break;
+            case this.States.NEW_LINE:
+                break;
+        }
+    }
+
+    enter(state) {
+        switch (state) {
+            case this.States.IDLE:
+                break;
+            case this.States.KEY_DOWN:
+                clearTimeout(this.timeout);
+                this.timeout = setTimeout(() => {
+                    this.transition(this.Trigger.TIMEOUT);
+                }, this.timeUnit * 3);
+                break;
+            case this.States.DOT:
+                clearTimeout(this.timeout);
+                dispatchEvent(this.dotEvent);
+                this.switch(this.States.KEY_UP);
+                break;
+            case this.States.DASH:
+                dispatchEvent(this.dashEvent);
+                break;
+            case this.States.KEY_UP:
+                this.timeout = setTimeout(() => {
+                    this.transition(this.Trigger.TIMEOUT);
+                }, this.timeUnit * 3);
+                break;
+            case this.States.CHARACTER_SPACE:
+                dispatchEvent(this.characterSpaceEvent);
+                this.timeout = setTimeout(() => {
+                    this.transition(this.Trigger.TIMEOUT);
+                }, this.timeUnit * 4);
+                break;
+            case this.States.WORD_SPACE:
+                dispatchEvent(this.wordSpaceEvent);
+                this.timeout = setTimeout(() => {
+                    this.transition(this.Trigger.TIMEOUT);
+                }, this.timeUnit * 10);
+                break;
+            case this.States.NEW_LINE:
+                dispatchEvent(this.newLineEvent);
+                this.switch(this.States.IDLE);
+                break;
+        }
+    }
+}
